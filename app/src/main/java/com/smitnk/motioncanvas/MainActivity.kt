e50044830319a37a6c29b28f4c735e44b0a3919a
smitnk <sumitnaikna29@gmail.com>
2026-09-18 12:40:28 +0530

    Import verticalScroll for settings panel

--- app/src/main/java/com/smitnk/motioncanvas/MainActivity.kt
+++ app/src/main/java/com/smitnk/motioncanvas/MainActivity.kt
@@ -22,6 +22,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.gestures.detectTransformGestures
 import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
