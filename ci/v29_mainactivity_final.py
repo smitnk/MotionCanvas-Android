aa839bef86518351b9cb100aa69a4dbb1a378cd8
smitnk <sumitnaikna29@gmail.com>
2026-09-19 22:56:30 +0530

    Add missing EditorScreen closing brace

--- ci/v29_mainactivity_final.py
+++ ci/v29_mainactivity_final.py
@@ -221,8 +221,6 @@ fun OnionSkinSettingsDialog''',
 needle = "@Composable\nfun OnionSkinSettingsDialog("
 pos = s.find(needle)
 if pos >= 0:
-    before = s[:pos]
-    if not before.rstrip().endswith("}"):
-        s = before.rstrip() + "\n}\n\n" + s[pos:]
+    s = s[:pos] + "}\n\n" + s[pos:]
 p.write_text(s)
 print("V29 MainActivity repair + diagnostics applied")
